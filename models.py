092a8059cb008c2bf14e018257553169dc313848
D3ci0 <cianodanilo@gmail.com>
2017-08-25 13:27:00 +0200

    Initial commit

---
+++ models.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema, fields
+from geoalchemy2 import Geometry
+
+app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"] = 'postgres://gyntkdkmnhdzjo:18e245b001185b2f9ed58193f7f0e68b2e26276c21d60bd90c3e6c896b22a1f4@ec2-107-20-186-238.compute-1.amazonaws.com:5432/d356a3m6qd1phc'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Registrazione(db.Model):
+    __tablename__ = 'registrazioni'
+
+    idreg = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.Text)
+    nome = db.Column(db.Text)
+    dettagli = db.Column(db.Text)
+    prezzo = db.Column(db.Float)
+    pos = db.Column(Geometry(geometry_type='POINT', srid=4326))
+    # foto(controllare conversione bytea)
+    data = db.Column(db.DateTime)
+    idutente = db.Column(db.Integer, db.ForeignKey('utenti.idutente'))
+    utente = db.relationship("Utente", back_populates = "idreg", lazy='joined')
+
+
+    def __init__(self, nome, tipo, dettagli, prezzo, pos, data, idutente):
+        self.nome = nome
+        self.tipo = tipo
+        self.dettagli = dettagli
+        self.prezzo = prezzo
+        self.pos = pos
+        self.data = data
+        self.idutente = idutente
+
+
+class Utente(db.Model):
+    __tablename__ = 'utenti'
+
+    idutente = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text)
+    password = db.Column(db.Text)
+    nome = db.Column(db.Text)
+    cognome = db.Column(db.Text)
+    idreg = db.relationship('Registrazione', back_populates = "utente")
+
+    def __init__(self, nome, cognome, username, password):
+        self.nome = nome
+        self.cognome = cognome
+        self.username = username
+        self.password = password
+
+
+
+class UtenteSchema(Schema):
+    idutente = fields.Int(dump_only=True)
+    username = fields.Str()
+    password = fields.Str()
+    nome = fields.Str()
+    cognome = fields.Str()
+
+class RegistrazioneSchema(Schema):
+    idreg = fields.Int(dump_only=True)
+    nome = fields.Str()
+    tipo = fields.Str()
+    dettagli = fields.Str()
+    prezzo = fields.Str()
+    data = fields.Date()
+    pos = fields.Str()
+    idutente = fields.Str()
+    utente = fields.Nested(UtenteSchema, allow_none=True)
+
+
+
+
+
+utente_schema = UtenteSchema()
+utenti_schema = UtenteSchema(many=True)
+registrazione_schema = RegistrazioneSchema()
+registrazioni_schema = RegistrazioneSchema(many=True)
---
+++ so2.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+from models import Registrazione, Utente, app, db, utenti_schema, registrazioni_schema, registrazione_schema, utente_schema
+from geoalchemy2 import *
+
+@app.route('/users', methods = ['GET'])
+def get_all_users():
+    utenti = Utente.query.all()
+    result = utenti_schema.dump(utenti)
+    return jsonify({'utenti': result.data})
+
+@app.route('/registrations', methods = ['GET'])
+def get_all_registrations():
+    registrazioni = Registrazione.query.all()
+    result = registrazioni_schema.dump(registrazioni)
+    return jsonify({'registrazioni' : result.data})
+
+@app.route('/reg_by_user', methods = ['GET','POST'])
+def get_reg_by_user():
+    idutente = request.get_json()
+    registrazione = Registrazione.query.filter_by(idutente = idutente).first()
+    result = registrazione_schema.dump(registrazione)
+    return jsonify({'registrazione' : result.data})
+
+@app.route('/save_reg', methods = ['POST'])
+def save_reg():
+    json = request.get_json()
+    registrazione = Registrazione(json['nome'], json['tipo'], json['dettagli'], json['prezzo'], json['point'], json['data'], json['idutente'])
+    registrazione.pos = WKTElement(registrazione.pos, 4326)
+    db.session.add(registrazione)
+    db.session.commit()
+    response = jsonify({'message' : 'Registration successfully saved'})
+    response.status_code = 200
+    return response
+
+@app.route('/auth_user', methods = ['GET','POST'])
+def auth_user():
+    json = request.get_json()
+    print(json)
+    username = json['username']
+    password = json['password']
+    utente = Utente.query.filter_by(username = username).first()
+    if utente :
+        if password == utente.password :
+            result = utente_schema.dump(utente)
+            return jsonify({'utente' : result.data})
+        else:
+            wrongp = jsonify({'message' : 'Wrong password'})
+            return wrongp
+    wrongu = jsonify({'message' : 'Wrong username'})
+    return wrongu
+
+
+
+if __name__ == '__main__':
+        app.run()
