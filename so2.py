06c84995503219f0e5180b4b98c71a0766d95a92
D3ci0 <cianodanilo@gmail.com>
2017-08-25 16:09:41 +0200

    JSON fix

--- so2.py
+++ so2.py
@@ -42,7 +42,7 @@ def auth_user():
     if utente :
         if password == utente.password :
             result = utente_schema.dump(utente)
-            return jsonify(result.data)
+            return jsonify(result)
         else:
             wrongp = jsonify({'message' : 'Wrong password'})
             return wrongp
