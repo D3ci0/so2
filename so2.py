3c64d3ae2edb934dd2cb643215863c3466e5f2eb
D3ci0 <cianodanilo@gmail.com>
2017-08-29 15:02:33 +0200

    Query fix

--- so2.py
+++ so2.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import jsonify, request
 from models import Registrazione, Utente, app, db, utenti_schema, registrazioni_schema, registrazione_schema, utente_schema
 from geoalchemy2 import *
 
@@ -17,9 +17,11 @@ def get_all_registrations():
 @app.route('/reg_by_user', methods = ['GET','POST'])
 def get_reg_by_user():
     idutente = request.get_json()
-    print(idutente)
     registrazione = Registrazione.query.filter_by(idutente = idutente)
-    result = registrazioni_schema.dump(registrazione)
+    if registrazione.count()>1 :
+        result = registrazioni_schema.dump(registrazione)
+    else :
+        result = registrazione_schema.dump(registrazione)
     #gestire quando ci sono una o più entry
     return jsonify(result.data)
 
@@ -36,7 +38,6 @@ def save_reg():
 @app.route('/auth_user', methods = ['GET','POST'])
 def auth_user():
     json = request.get_json()
-    print(json)
     username = json['username']
     password = json['password']
     utente = Utente.query.filter_by(username = username).first()
